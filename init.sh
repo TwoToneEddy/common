b66028299ebceca4264d7a7dbe7498efaf06e770
lee <lee.hudson1384@yahoo.co.uk>
2020-02-14 19:04:34 +0000

    adapted for mint

--- init.sh
+++ init.sh
@@ -16,10 +16,10 @@ pip install -U platformio
 git clone git@github.com:TwoToneEddy/TH3D-Unified-U1.R2.git
 git clone git@github.com:TwoToneEddy/quadSettings.git
 sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ~/Downloads/VMware-Player-15.5.1-15018445.x86_64.bundle
+sudo sh ~/Downloads/VMware-Player-15.5.1-15018445.x86_64.bundle
 sudo dpkg -i ~/Downloads/betaflight-configurator_10.6.0_amd64.deb
-sudo tar -C /opt/ -xzfv ~/Downloads/BLHeliSuite32xl.tar.gz
-sudo tar -C /opt/ -xzfv ~/Downloads/ultimaker.tar.gz
-sudo tar -C /opt/ -xzfv ~/Downloads/jetbrains-toolbox-1.16.6319.tar.gz
+sudo tar -C /opt/ -xzf ~/Downloads/BLHeliSuite32xl.tar.gz
+sudo tar -C /opt/ -xzf ~/Downloads/ultimaker.tar.gz
+sudo tar -C /opt/ -xzf ~/Downloads/jetbrains-toolbox-1.16.6319.tar.gz
 
 
